9284b2c2392542e0678688f2f83a7b83a8ff3d3b
Hyomoto <dragon_idea@yahoo.com>
2026-08-09 22:43:33 -0400

    an extremely dumb path error

--- clean.bat
+++ clean.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
-cd /d "%~dp0.."
+cd /d "%~dp0"
 python scripts\clean_kit.py %*
 endlocal
--- pack_release.bat
+++ pack_release.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-cd /d "%~dp0.."
+cd /d "%~dp0"
 python scripts\clean_kit.py --deep-vendor
 if errorlevel 1 exit /b 1
 python scripts\pack_release.py %*
